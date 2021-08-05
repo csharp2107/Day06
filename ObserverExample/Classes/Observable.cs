b39d5862149297433fa52a9c10f0d5564c71262b
kurs <kurs@DESKTOP-V3HDKPQ>
2021-08-05 14:04:51 +0200

    Add project files.

---
+++ ObserverExample/Classes/Observable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverExample.Classes
+{
+    class Observable<T>
+    {
+        private List<IObserver<T>> observers = new List<IObserver<T>>();
+        private T subject;
+
+        public T Subject {
+            get => subject; 
+            set
+            {
+                subject = value;
+                Notify();
+            }
+        }
+
+        private void Notify()
+        {
+            foreach (var observer in observers)
+            {
+                observer.Update(subject);
+            }
+        }
+
+        public void Register(IObserver<T> observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void Unregister(IObserver<T> observer)
+        {
+            observers.Remove(observer);
+        }
+    }
+}
---
+++ ObserverExample/StockDummySimulator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverExample
+{
+    class StockDummySimulator : IEnumerable<Stock>
+    {
+        private Random rnd = new Random();
+        private string[] names = { "APL", "GOG", "IBM" };
+
+        public IEnumerator<Stock> GetEnumerator()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                string n = names[rnd.Next(0, 3)];
+                double v = rnd.Next(1, 101) + rnd.NextDouble();
+                yield return new Stock(n, v);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Stock
+    {
+        public string Name;
+        public double Value;
+        public Stock(string name, double value)
+        {
+            Name = name; Value = value;
+        }
+    }
+}
