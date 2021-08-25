74dc5f93a3f1cda9265e8ee3e9154999caabefd8
kurs <kurs@DESKTOP-V3HDKPQ>
2021-08-25 09:25:52 +0200

    observable collection

---
+++ ObserverCollection/ObsColl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverCollection
+{
+    class Employee
+    {
+        public String Name { get; set; }
+        public bool? IsManager { get; set; } 
+        public double? Salary { get; set; }
+        public DateTime? StartAt { get; set; }
+    }
+
+    class EmployeeList : ObservableCollection<Employee>
+    {
+        public EmployeeList()
+        {
+            this.Add(new Employee() { Name="John"  }  );
+            this.Add(new Employee() { Name = "Elvis", Salary=99999.99, IsManager=true });
+            this.Add(new Employee() { Name = "Eleonora", Salary = 99999.99, IsManager = false });
+            this.Add(new Employee() { Name = "Emil", Salary = 49999.99, IsManager = true });
+        }
+
+        public IEnumerable<Employee> GetEmpByNameAndManager(string name, bool? isManager)
+        {
+            var res = this.Where(x =>
+               x.Name.StartsWith(name, StringComparison.OrdinalIgnoreCase) &&
+               x.IsManager == isManager).AsEnumerable<Employee>();
+            return res;
+        }
+    }
+}
---
+++ ObserverCollection/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeList empList = new EmployeeList();
+            empList.CollectionChanged += EmpList_CollectionChanged;
+            empList.Add(new Employee() { Name = "Peter" });
+            //empList.RemoveAt(1);
+            empList.Insert(0, new Employee() { Name = "John" });
+
+            var result = empList.GetEmpByNameAndManager("E", true);
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.Name} - {item.IsManager}");
+            }
+        }
+
+        private static void EmpList_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            Console.WriteLine(e.Action);
+        }
+    }
+}
