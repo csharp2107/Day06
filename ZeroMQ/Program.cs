34806a6f5244a136ecf7f91c3390101aaa7316db
kurs <kurs@DESKTOP-V3HDKPQ>
2021-08-25 12:04:15 +0200

    final update

--- ZeroMQ/Program.cs
+++ ZeroMQ/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using NetMQ;
 using NetMQ.Sockets;
@@ -12,22 +13,42 @@ namespace ZeroMQ
     {
         static void Main(string[] args)
         {
-            // implementing publisher
-            Random rnd = new Random();
-            using (var pubSocket = new PublisherSocket())
-            {
-                // bind publisher to TCP socket
-                pubSocket.Bind("tcp://*:12345");
-                for (int i = 0; i < 100; i++)
+            new Thread( ()=> {
+
+                // implementing publisher
+                Console.WriteLine("Publisher is starting...");
+                Random rnd = new Random();
+                using (var pubSocket = new PublisherSocket())
                 {
-                    string topic = "topicA";
-                    if (rnd.NextDouble() > 0.5)
-                        topic = "topicB";
-                    string msg = $"{topic} - {i}";
-                    pubSocket.SendMoreFrame(topic).SendFrame(msg);
-                    Console.WriteLine($"PUBLISHER: {msg}");
+                    // bind publisher to TCP socket
+                    pubSocket.Bind("tcp://*:12345");
+                    for (int i = 0; i < 100; i++)
+                    {
+                        string topic = rnd.NextDouble() > 0.5 ? "topicA" : "topicB";
+                        string msg = $"{topic} - {i}";
+                        pubSocket.SendMoreFrame(topic).SendFrame(msg);
+                        Console.WriteLine($"PUBLISHER: {msg}");
+                        Thread.Sleep(1000);
+                    }
+                }
+
+            } ).Start();
+
+            using (var subSocket = new SubscriberSocket())
+            {
+                subSocket.Connect("tcp://localhost:12345"); //connecting to publisher
+                //subSocket.Subscribe("topicA"); // how to subscribe messages from desired topic
+                subSocket.SubscribeToAnyTopic(); // subSocket.Subscribe("");
+                Console.WriteLine("Receiving messages...");
+                while (true)
+                {
+                    string topic = subSocket.ReceiveFrameString(); // receiving topic
+                    string msg = subSocket.ReceiveFrameString();
+                    Console.WriteLine($"RECEIVED: {topic} - {msg}");
                 }
             }
+
+            Console.ReadKey();
 
         }
     }
